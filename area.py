1edfe4f5a1ca2b2d630d4d7d880bb9574c019409
Ricardo <ricardorivero@gmail.com>
2024-10-15 20:05:39 -0300

    area.py

---
+++ area.py
@@ -0,0 +1,5 @@
+import math 
+def area_cuadrado(base,altura):
+    return base*altura
+def area_circulo(radio):
+    return math.pi **2
---
+++ lista.py
@@ -0,0 +1,5 @@
+def medida_lista(numeros):
+    return len (numeros)
+lista_ejemplos=[1,2,3,4,5]
+medida=medida_lista(lista_ejemplos)
+print(f"medida la lista:{medida}")
